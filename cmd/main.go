1dc85af61c0f39c91d0860dd116ac2a6d98c00c9
Michael Skuratovich <mishuk.sk@gmail.com>
2020-12-23 20:07:41 +0300

    added basic server startup

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const ShutdownTimeout = time.Second * 5
+
+func main() {
+	mux := http.NewServerMux()
+
+	// register mux handlers
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("HTTP server ListenAndServe: %s", err)
+		}
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT)
+
+	// wait for os signal
+	<-sigCh
+	log.Println("os interrrupt  - shutting down")
+
+	ctx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("sutdown error: %s", err)
+	}
+	log.Println("server stopped")
+}
